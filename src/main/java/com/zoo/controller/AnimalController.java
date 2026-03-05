fd83f319ec5fa2d0178ee2643dd9c8896b9fd3e3
Marco Diaz <marcoalejandro.diazcastaneda@gmail.com>
2026-03-04 18:32:37 -0600

    split backend and frontend

--- src/main/java/com/zoo/controller/AnimalController.java
+++ src/main/java/com/zoo/controller/AnimalController.java
@@ -1,8 +1,7 @@
 package com.zoo.controller;
 
-import com.zoo.dto.AnimalDTO;
-import com.zoo.model.Animal;
-import com.zoo.service.AnimalService;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.zoo.dto.AnimalDTO;
+import com.zoo.model.Animal;
+import com.zoo.service.AnimalService;
 
 @RestController
 @RequestMapping("/api/animales")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://eldmark.github.io")
 public class AnimalController {
 
     private final AnimalService service;
